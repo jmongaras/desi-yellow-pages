f7c88bdaf096b83017cb31f16d62e54515f26a0e
Jason Mongaras <jmongaras@gmail.com>
2021-03-23 11:03:21 +0530

    init commit
    init commit

---
+++ resources/js/Jetstream/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+            <div>
+                <jet-application-logo class="block h-12 w-auto" />
+            </div>
+            <div class="mt-8 text-2xl">
+                Welcome to Desi Yellow Pages!
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import JetApplicationLogo from '@/Jetstream/ApplicationLogo'
+
+    export default {
+        components: {
+            JetApplicationLogo,
+        },
+    }
+</script>
